60e00cc7d40956d9cb2cc71e72fb5ebadc85491e
EvgenyShokotko <103266879+EvgenyShokotko@users.noreply.github.com>
2022-09-14 13:44:40 +0200

    start

--- src/index.js
+++ src/index.js
@@ -1,34 +1,3 @@
 import './css/styles.css';
-import { TEMPLATE } from "./part";
-import API from "./api-service";
 
 const DEBOUNCE_DELAY = 300;
-
-const body = document.querySelector('.pokemons');
-const btn = document.querySelector('.search-button');
-const form = document.querySelector('.search-form');
-form.style.display = 'flex';
-btn.style.marginLeft = '30px';
-form.addEventListener('submit', onSearchFormClick);
-
-function onSearchFormClick(event) {
-    event.preventDefault();
-    const form = event.currentTarget;
-    const input = form.elements.search.value;
-     
-    API.fetchPocemon(input)
-    .then(renderPokemonCard)
-    .catch(onFetchError)
-    .finally(() => form.reset());
-    
-}
-
-function renderPokemonCard(pokemon) {
-    const markup = TEMPLATE(pokemon);
-    body.innerHTML = markup;
-}
-
-function onFetchError(error) {
-    console.log(error);
-    alert('УПС, мы не нашли такого покемона');
-}
